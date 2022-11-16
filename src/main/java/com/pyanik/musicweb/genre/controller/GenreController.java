e7b9b3de25478b522200ad8045321974e188d19e
pyanik <pyanik@users.noreply.github.com>
2022-11-16 19:39:43 +0100

    add get method for genre albums list

--- src/main/java/com/pyanik/musicweb/genre/controller/GenreController.java
+++ src/main/java/com/pyanik/musicweb/genre/controller/GenreController.java
@@ -1,21 +1,43 @@
 package com.pyanik.musicweb.genre.controller;
 
+import com.pyanik.musicweb.album.dto.AlbumDto;
+import com.pyanik.musicweb.album.service.AlbumService;
 import com.pyanik.musicweb.genre.dto.GenreDto;
 import com.pyanik.musicweb.genre.service.GenreService;
+import com.pyanik.musicweb.homepage.service.HomePageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-class GenreController {
+public class GenreController {
 
     private final GenreService genreService;
 
+    private final AlbumService albumService;
+
     @GetMapping("/genres")
-    public List<GenreDto> getGenres() {
-        return genreService.getGenres();
+    public ResponseEntity<List<GenreDto>> getGenres() {
+        return ResponseEntity.ok(genreService.getGenres());
+    }
+
+    @GetMapping("/genres/{name}")
+    public ResponseEntity<Page<AlbumDto>> getGenreAlbums(@PathVariable(value = "name") String genreName,
+                                                         @PageableDefault(size = 5) Pageable pageable) {
+        genreService.findGenreByName(genreName)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        Page<AlbumDto> albumDtos = albumService.getAlbumsByGenreName(pageable, genreName);
+        return ResponseEntity.ok(albumDtos);
     }
 }
