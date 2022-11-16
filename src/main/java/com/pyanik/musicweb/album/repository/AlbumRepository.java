e76e490f562846cd6ee6a12946109a9aee5ae945
pyanik <pyanik@users.noreply.github.com>
2022-11-16 19:46:32 +0100

    add get method for genre albums list

--- src/main/java/com/pyanik/musicweb/album/repository/AlbumRepository.java
+++ src/main/java/com/pyanik/musicweb/album/repository/AlbumRepository.java
@@ -1,9 +1,17 @@
 package com.pyanik.musicweb.album.repository;
 
 import com.pyanik.musicweb.common.model.Album;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
+
+    Page<Album> findAllByPromotedIsTrue(Pageable pageable);
+
+    Page<Album> findAllByGenre_NameIgnoreCase(String genreName, Pageable pageable);
 }
--- src/main/java/com/pyanik/musicweb/album/service/AlbumService.java
+++ src/main/java/com/pyanik/musicweb/album/service/AlbumService.java
@@ -1,16 +1,33 @@
 package com.pyanik.musicweb.album.service;
 
+import com.pyanik.musicweb.album.dto.AlbumDto;
 import com.pyanik.musicweb.album.mapper.AlbumModelMapper;
 import com.pyanik.musicweb.album.repository.AlbumRepository;
+import com.pyanik.musicweb.common.model.Album;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public
-class AlbumService {
+public class AlbumService {
 
     private final AlbumRepository albumRepository;
+
     private final AlbumModelMapper albumModelMapper;
 
+    public AlbumDto getAlbumById(Long id) {
+        Album album = albumRepository.findById(id).orElseThrow();
+        return albumModelMapper.mapAlbumEntityToAlbumDto(album);
+    }
+
+    public Page<AlbumDto> getAlbumsByGenreName(Pageable pageable, String genreName) {
+        Page<Album> albums = albumRepository.findAllByGenre_NameIgnoreCase(genreName, pageable);
+        List<AlbumDto> albumDtos = albums.stream().map(albumModelMapper::mapAlbumEntityToAlbumDto).toList();
+        return new PageImpl<>(albumDtos, pageable, albums.getTotalElements());
+    }
 }
